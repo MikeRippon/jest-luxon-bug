9e58e0a38613082bd79fd8928e5acec3fc0e0f92
Mike Rippon <code@littlemike.co.uk>
2024-05-01 14:49:13 -0600

    Simplify test cases

--- date-check.test.js
+++ date-check.test.js
@@ -9,25 +9,12 @@ it('all looks fine to start with', () => {
 })
 
 it('but any failure involving a date time...', async () => {
-    expect(millenium).toEqual(DateTime.fromObject({ year: 1234 }))
+    // Bug can be resolved by removing this expectation, or comparing to something other than an object (e.g. a string)
+    expect(millenium).toEqual({})
 })
 
 it('...seems to somehow "taint" that specific datetime', () => {
     const localMillenium = DateTime.fromObject({ year: 2000 })
 
     expect(localMillenium).toEqual(millenium) // Fails (serializes to same string)
-})
-
-it('...including any derived date times', () => {
-    const nextDay1 = millenium.plus({ days: 1 })
-    const nextDay2 = DateTime.fromObject({ year: 2000 }).plus({ days: 1 })
-
-    expect(nextDay1).toEqual(nextDay2) // Fails (serializes to same string)
-})
-
-it('..but dates derived from "tainted" date time can still be compared with each other', () => {
-    const nextDay1 = millenium.plus({ days: 1 })
-    const nextDay2 = millenium.plus({ days: 1 })
-
-    expect(nextDay1).toEqual(nextDay2) // OK
 })
