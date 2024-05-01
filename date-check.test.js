5d7372946db703521f7cb5eb9a4c3648a1342648
Mike Rippon <code@littlemike.co.uk>
2024-05-01 14:51:47 -0600

    Even simpler

--- date-check.test.js
+++ date-check.test.js
@@ -1,20 +1,17 @@
 const DateTime = require("luxon").DateTime
 
-const millenium = DateTime.fromObject({ year: 2000 })
+const aNewDateTime = () => DateTime.fromObject({ year: 2000 })
+
+const sharedDate = aNewDateTime()
 
 it('all looks fine to start with', () => {
-    const localMillenium = DateTime.fromObject({ year: 2000 })
-
-    expect(localMillenium).toEqual(millenium)
+    expect(sharedDate).toEqual(aNewDateTime())
 })
 
-it('but any failure involving a date time...', async () => {
-    // Bug can be resolved by removing this expectation, or comparing to something other than an object (e.g. a string)
-    expect(millenium).toEqual({})
+it('but any failure involving a date time + object...', async () => {
+    expect(sharedDate).toEqual({})
 })
 
 it('...seems to somehow "taint" that specific datetime', () => {
-    const localMillenium = DateTime.fromObject({ year: 2000 })
-
-    expect(localMillenium).toEqual(millenium) // Fails (serializes to same string)
+    expect(sharedDate).toEqual(aNewDateTime())
 })
